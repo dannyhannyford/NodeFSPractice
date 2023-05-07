d32b15b376befbf54de7b04e8fa832586f531ef7
dannyhannyford <dannyhannyford@gmail.com>
2023-05-06 21:02:20 -0700

    use watch and read the changes within command.txt with .read()

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const fs = require('fs/promises');
 
 (async () => {
-  try {
-    await fs.copyFile('text.txt', 'copied-promise.txt');
-  } catch (error) {
-    console.log(error)
+  // reads open file
+  const commandFileHandler = await fs.open('./command.txt', 'r')
+
+  const watcher = fs.watch('./command.txt');
+
+  for await (const event of watcher) {
+    if (event.eventType === 'change') {
+      console.log('thie file was changed');
+      // read the content
+
+      //get the size of our file
+      const fileSize = (await commandFileHandler.stat()).size;
+      const buff = Buffer.alloc(fileSize)
+      //position in the buffer
+      const offset = 0;
+      const length = buff.byteLength;
+      // position in the file
+      const position = 0;
+
+      console.log(await commandFileHandler.stat());
+      const content = await commandFileHandler.read(buff, offset, length, position);
+      console.log(content)
+    }
   }
 })();
 
