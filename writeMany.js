4a9622d03bfeb8d0c8c8c04ecc3b053eb0d7d6ca
dannyhannyford <dannyhannyford@gmail.com>
2023-05-08 20:14:58 -0700

    use writeSync to avoid spamming the event loop

--- writeMany.js
+++ writeMany.js
@@ -6,6 +6,7 @@ const fs = require('node:fs');
     // don't create the buffer every time in the for loop to save time
     const buff = Buffer.from( ` a `, 'utf-8');
     for(let i = 0; i < 1000000; i++) {
+      // fs.write pushes a lot of events to the event loop
       fs.writeSync(fd, buff);
     }
     console.timeEnd('writeMany')
